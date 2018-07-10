108a4cfd0894563da6afaff799ccd32fb69eb215
DerekHawkins <38869302+DerekHawkins@users.noreply.github.com>
2018-07-10 10:05:36 -0400

    Update colors_chooser.py

--- colors_chooser.py
+++ colors_chooser.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import random
 
-colors = pd.read_excel("BenjaminMooreColors.xlsx")
+colors = pd.read_excel("BenjaminMooreColors.xlsx") #route this to where the downloaded file is on your computer
 colorName = colors["Colors"]
 
 random.choice(colorName)
